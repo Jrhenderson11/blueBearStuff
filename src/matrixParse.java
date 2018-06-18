648a15f4129a1051d02af9569e4495c1530ad686
James <jrhenderson11@gmail.com>
2018-06-18 21:19:18 +0100

    removed sysouts

--- src/matrixParse.java
+++ src/matrixParse.java
@@ -15,14 +15,14 @@ public class matrixParse {
 			
 			String line = lineA.substring(1, lineA.length()-1);
 			
-			System.out.println(line);
+			//System.out.println(line);
 			
 			String[] parts = line.split("\\], \\[");
 			for (int colindex =0; colindex < parts.length; colindex++) {
 				String row = parts[colindex].replace("]", "").replace("[", "");
 				for (String number : row.split("\\), \\(")) {
 					number = number.replace(")", "").replace("(", "");
-					System.out.println("number=" + number);
+					//System.out.println("number=" + number);
 					int n1 = Integer.parseInt(number.replaceAll(" ", "").split(",")[0]);
 					double n2 = Double.parseDouble(number.replaceAll(" ", "").split(",")[1]);
 					matrix[index][n1][colindex] = n2;
@@ -32,14 +32,14 @@ public class matrixParse {
 		}
 		
 		
-		System.out.println(matrix);
-		for (int y=0;y<18;y++) {
-			for (int x=0;x<18;x++) {
-				System.out.print(matrix[0][x][y] + ", ");
-			}
-			System.out.println();
-			
-		}
+		//System.out.println(matrix);
+//		for (int y=0;y<18;y++) {
+//			for (int x=0;x<18;x++) {
+//				//System.out.print(matrix[0][x][y] + ", ");
+//			}
+//			System.out.println();
+//			
+//		}
 		
 		return matrix;
 		
